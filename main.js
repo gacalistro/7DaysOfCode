51096745f6e8439ec3b5761807fc1f5453b6a0e4
Gabriel Calistro <gahcalistro@gmail.com>
2022-05-16 07:59:54 -0300

    active style at navigation menu + scrollReveal lib

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ onScroll();
 function onScroll() {
   showNavOnScroll();
   backToTopButtonShow();
+  activeMenuAtCurrentSection(home);
+  activeMenuAtCurrentSection(carreer);
+  activeMenuAtCurrentSection(comments);
 }
 
 function showNavOnScroll() {
@@ -29,3 +32,42 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove("menu-expanded");
 }
+
+function activeMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+  const sectionBottom = sectionTop + sectionHeight;
+  const sectionId = section.getAttribute("id");
+
+  const passedTop = targetLine >= sectionTop;
+  const passedBottom = targetLine >= sectionBottom;
+  const sectionBoundaries = passedTop && !passedBottom;
+
+  const menuElement = document.querySelector(
+    `#navigation .menu a[href*=${sectionId}]`
+  );
+
+  menuElement.classList.remove("active");
+
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
+}
+
+ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+}).reveal(`
+  #home,
+  #home .user,
+  #home .cards,
+  #home .card,
+  #carreer,
+  #carreer .carreers,
+  #carreer .jobs,
+  #carreer .job,
+  #comments,
+  footer`);
--- readme.md
+++ readme.md
@@ -20,6 +20,8 @@ The project has been developed with these technologies:
 
 OptimusTech is a responsive page in One Page format, and it was made in order to be used for new hires of a IT business. This page features these following sections: Navigation, Home, Carreers, Comments and Footer.
 
+This project was developed for educational purposes during the 7days Of Code event powered by [Alura](https://7daysofcode.io/).
+
 ## <img src="https://cdn-icons-png.flaticon.com/512/922/922699.png" height="25px" width="25px"/> Layout
 
 Avaliable on [this link](https://www.figma.com/file/wisiMC9uItrmLguIBqDjBr/7daysOfCode-HTML-CSS). For more details, make sure you have an account on [<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" height="16px" width="16px"/> Figma](https://figma.com).
